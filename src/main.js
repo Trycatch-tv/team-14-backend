bbe2419dd7a2f74bba516e06f3b9def6db49c648
Stiven Medina <stvnm33@gmail.com>
2023-04-08 16:49:12 -0500

    Añadiendo el resto de rutas y contraldores

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import express from "express";
 import routerDirectores from './routes/directores.routes.js'
 import routerGeneros from './routes/generos.routes.js'
 import routerCategorias from './routes/categorias.routes.js'
+import routerUsuarios from './routes/usuarios.routes.js'
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use(routerDirectores)
 app.use(routerGeneros)
 app.use(routerCategorias)
+app.use(routerUsuarios)
+
 
 app.listen(3000, ()=>{
     console.log(':: Server running on port 3000 ::')
